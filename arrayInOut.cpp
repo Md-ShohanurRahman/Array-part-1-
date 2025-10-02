9c0ec06d3dfd9cad83dafbe413ccc3d939861c8c
Md-ShohanurRahman <shohanurrahmanibnaymizan@gmail.com>
2025-10-02 16:17:02 +0600

    first commit of array part 1

---
+++ arrayInOut.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout <<"enter the size of the array "<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<", ";
+    }
+    cout<<endl;
+    
+    return 0;
+}
